1c4fe81c003bcb561e5c1e3864698006e7a0d9b4
mhonorio06 <136056924+mhonorio06@users.noreply.github.com>
2024-02-07 21:10:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
 document.querySelector("#collection-form").addEventListener("submit", submitEvent)
 
 async function getData() {
-    const response = await fetch("http://localhost:3000/charData");
+    response = await fetch("http://localhost:3000/charData");
     main = await response.json();
     main.forEach(figure => renderFigure(figure));
 }
 
 function submitEvent(e) {
     const form = document.querySelector("#collection-form")
-    
     e.preventDefault()
     let newObj = {
         id: (main.length + 1).toString(),
@@ -40,6 +39,7 @@ function renderFigure(figure) {
     </div>
     <div class="buttons">
     <button id="increase"> Increase Bid $100 </button>
+    <button id="buyNow"> Buy Now </button>
     </div>
     `
     card.querySelector("#increase").addEventListener("click", () => {
@@ -47,8 +47,10 @@ function renderFigure(figure) {
         card.querySelector("span").textContent = figure.price
         updatePrice(figure)
     })
+    card.querySelector("#buyNow").addEventListener("click", () => {
+        card.remove();
+        buyNow(figure.id)
 
-    
     document.querySelector("#collection-container").appendChild(card)
     }
 function createNew(newObj) {
@@ -72,6 +74,14 @@ function updatePrice(newObj) {
     })
     .then(res => res.json())
     .then(figure => console.log(figure))
+}
+function buyNow(id) {
+    fetch(`htttp://localhost:3000/charData/`${id}, {
+        method: 'DELETE',
+        headers: {
+            'content-type' : "application/json'
+        },
+    })
 }
 getData()
 })
